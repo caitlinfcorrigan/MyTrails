d6f445690eeddbd7116aa0bc52e9ca9a44108ed9
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-10-27 14:20:56 -0700

    creates controllers/trails.js

---
+++ controllers/trails.js
@@ -0,0 +1,33 @@
+const Trail = ('../models/trail');
+const Review = ('../models/review');
+
+module.exports = {
+    index,
+    show,
+    create
+}
+
+async function create(req, res) = {
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body.[key];
+    }
+    try {
+        const review = await Review.create(req.body);
+        trail.reviews.push(review._id);
+        res.redirect(`/trails/${trail._id}`);
+    } catch (err) {
+        console.log(err);
+        res.render(`/trails/${trail._id}`, { errorMsg: err.message });
+    }
+}
+
+async function show(req, res) = {
+    const trail = await Trail.findById(req.params.id).populate('review');
+    const reviews = await Review.find({ _id: { $nin: trail.review} }).sort('rating');
+    res.render('trails/show', { title: `${trail.name}'s Reviews`, trail, reviews});
+}
+
+async function index(req, res) = {
+    const trails = await Trail.find({});
+    res.render('trails/index', { title: 'All Trails', trails });
+}
