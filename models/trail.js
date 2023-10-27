86d01bd819329265a16c99020ac732f1e59f7f67
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-10-27 12:06:31 -0700

    creates trail schema and model

---
+++ models/trail.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const trailSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    distance: Number,
+    streetAddress: String,
+    city: String,
+    state: {
+        type: String,
+        enum: ['AL','AK','AZ','AR','AS','CA','CO','CT','DE','DC','FL','GA','GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','MP','OH','OK','OR','PA','PR','RI','SC','SD','TN','TX','TT','UT','VT','VA','VI','WA','WV','WI','WY']
+    },
+    zip: {
+        type: Number,
+        min: 5,
+        max: 5
+    },
+    reviews: {
+        type: Schema.Types.ObjectId,
+        ref: 'reviewSchema'
+    }
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model('trail', trailSchema)
