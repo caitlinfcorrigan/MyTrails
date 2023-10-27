b1b4b14c66d23c6e48bd1e4745187f5b2106a199
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-10-27 12:30:43 -0700

    creates reviews router

---
+++ routes/reviews.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const reviewsCtrl = require('../controllers/reviews')
+
+// GET /reviews -- User views My Reviews
+router.get('/reviews', reviewsCtrl.index);
+
+// PUT /reviews/:id  -- User modifies their review
+router.put('/reviews/:id', reviewsCtrl.update);
+
+// DELETE /reviews/:id -- User deletes their review
+router.delete('/reviews/:id', reviewsCtrl.delete);
